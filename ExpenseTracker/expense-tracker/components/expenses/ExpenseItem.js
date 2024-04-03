adac5be76ca44672cb1e8367842e952416535836
roxanap33 <roxana.ciocian@principal33.com>
2024-04-03 21:06:50 +0300

    135: Add function for date formatting

--- ExpenseTracker/expense-tracker/components/expenses/ExpenseItem.js
+++ ExpenseTracker/expense-tracker/components/expenses/ExpenseItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
+import { getFormattedDate } from "../../util/date";
 
 export default function ExpenseItem({ description, date, amount }) {
   return (
@@ -10,10 +11,10 @@ export default function ExpenseItem({ description, date, amount }) {
           <Text style={[styles.textBase, styles.description]}>
             {description}
           </Text>
-          <Text style={styles.textBase}>{date.toString()}</Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
-          <Text style={styles.amount}>{amount}</Text>
+          <Text style={styles.amount}>{amount.toFixed(2)}</Text>
         </View>
       </View>
     </Pressable>
@@ -26,7 +27,7 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     backgroundColor: GlobalStyles.colors.primary500,
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
     borderRadius: 6,
     shadowColor: GlobalStyles.colors.gray500,
     shadowRadius: 4,
@@ -48,6 +49,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 4,
+    minWidth: 80,
   },
   amount: {
     color: GlobalStyles.colors.primary500,
