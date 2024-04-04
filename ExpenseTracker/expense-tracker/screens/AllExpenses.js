1975aa3448f52c4f4309ba847591068ace6c5462
roxanap33 <roxana.ciocian@principal33.com>
2024-04-04 15:19:36 +0300

    144: Create a context for expenses and add a function to get the recent expenses

--- ExpenseTracker/expense-tracker/screens/AllExpenses.js
+++ ExpenseTracker/expense-tracker/screens/AllExpenses.js
@@ -1,6 +1,12 @@
 import { Text } from "react-native";
 import ExpensesOutput from "../components/expenses/ExpensesOutput";
+import { useContext } from "react";
+import { ExpensesContext } from "../store/expenses-context";
 
 export default function AllExpenses() {
-  return <ExpensesOutput expensesPeriod="Total" />;
+  const expensesCtx = useContext(ExpensesContext);
+
+  return (
+    <ExpensesOutput expensesPeriod="Total" expenses={expensesCtx.expenses} />
+  );
 }
--- ExpenseTracker/expense-tracker/screens/RecentExpenses.js
+++ ExpenseTracker/expense-tracker/screens/RecentExpenses.js
@@ -1,6 +1,20 @@
 import { Text } from "react-native";
 import ExpensesOutput from "../components/expenses/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
+import { useContext } from "react";
+import { getDateMinusDay } from "../util/date";
 
 export default function RecentExpenses() {
-  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
+  const expensesCtx = useContext(ExpensesContext);
+
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDay(today, 7);
+
+    return expense.date > date7DaysAgo;
+  });
+
+  return (
+    <ExpensesOutput expensesPeriod="Last 7 Days" expenses={recentExpenses} />
+  );
 }
